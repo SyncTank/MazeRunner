ebba01e78863c55f721748bfe9bb0860fd18e0d1
Rudy <30980904+SyncTank@users.noreply.github.com>
2024-02-25 18:01:00 -0500

    Added types to methods & arguements. Moved the draw cell and calling of the break walls method

--- WindowUI.py
+++ WindowUI.py
@@ -11,19 +11,19 @@ class MainWindow:
         self.isRunning = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
         self.isRunning = True
         while self.isRunning:
             self.redraw()
 
-    def close(self):
+    def close(self) -> None:
         self.isRunning = False
 
-    def draw_line(self, line, colour="black"):
+    def draw_line(self, line, colour="black") -> None:
         line.draw(self._canvas, colour)
 
 
@@ -34,10 +34,10 @@ class Point:
 
 
 class Line:
-    def __init__(self, point1, point2):
+    def __init__(self, point1, point2) -> None:
         self._point1 = point1
         self._point2 = point2
 
-    def draw(self, canvas, fill_colour):
+    def draw(self, canvas, fill_colour) -> None:
         canvas.create_line(self._point1.x, self._point1.y, self._point2.x, self._point2.y, fill=fill_colour, width=2)
         canvas.pack(fill=BOTH, expand=1)
